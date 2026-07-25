e2c076669fd582fcd45c5deaead24ab58ee88f26
Humming-Moon-afk <milian.loechner@gmail.com>
2026-07-25 19:10:43 +0200

    feat(backend): implement automated Ollama patient generator with DB integration

--- backend/src/main/java/com/med/backend/OllamaScheduler.java
+++ backend/src/main/java/com/med/backend/OllamaScheduler.java
@@ -8,6 +8,8 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Component
 public class OllamaScheduler {
     @Autowired
@@ -20,7 +22,21 @@ public class OllamaScheduler {
         request.put("model", "llama3.2");
         request.put("stream", false);
         request.put("prompt", "Generiere einen zufälligen deutschen Patienten im JSON-Format mit den Feldern: patientID, firstName, lastName, gender, birthDate, managingClinic, Sonstiges. Antworte NUR mit reinem JSON.");
-        
+
+        Map response = restTemplate.postForObject(url, request, Map.class);
+        if(response!=null && response.containsKey("response")) {
+            String jsonString = (String) response.get("response");
+            System.out.println("Antwort " + jsonString);
+            ObjectMapper mapper = new ObjectMapper();
+            try {
+            PatientEntity entity = mapper.readValue(jsonString, PatientEntity.class);
+            patientRepository.save(entity);
+            System.out.println("Erfolg");
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
+            
+        }
         
     }
 }
